95f432863588a5d9daee7d159495355d5a434a52
MarinMcCullough <mgm7cp@umsystem.edu>
2025-04-15 17:36:14 -0500

    updates to javascript

--- js/gallery.js
+++ js/gallery.js
@@ -2,9 +2,9 @@
 
 var photoDivs = document.getElementsByClassName("gallery-image");
 
-var nextButton = document.getElementById("button_next");
+var button_next = document.getElementById("button_next");
 
-var previousButton = document.getElementById("button_previous");
+var button_previous = document.getElementById("button_previous");
 
 var photoNumber = document.getElementById("stepper");
 
@@ -14,7 +14,7 @@ photoDivs[currentPhotoNumber].classList.remove('hideThis');
 
 photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
 
-nextButton.addEventListener('click', function() {
+button_next.addEventListener('click', function() {
   photoDivs[currentPhotoNumber].classList.add("hideThis");
   currentPhotoNumber = currentPhotoNumber + 1;
 
@@ -28,7 +28,7 @@ nextButton.addEventListener('click', function() {
 
 });
 
-previousButton.addEventListener('click', function() {
+button_previous.addEventListener('click', function() {
   photoDivs[currentPhotoNumber].classList.add("hideThis");
   currentPhotoNumber = currentPhotoNumber - 1;
 
