0ce6b8745c9cc8c7276dc05394fe4fe8eacf44de
MarinMcCullough <mgm7cp@umsystem.edu>
2025-05-08 12:33:54 -0500

    updates

--- js/gallery.js
+++ js/gallery.js
@@ -26,7 +26,6 @@ nextButton.addEventListener('click', function() {
 
   photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
 
-});
 
 previousButton.addEventListener('click', function() {
   photoDivs[currentPhotoNumber].classList.add("hideThis");
